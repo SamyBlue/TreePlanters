9f3e39bef5f33b32b77dd6f6440f6958dfbf464e
Rachel Kendal <rachelkendal@Rachels-MacBook-Air.local>
2021-04-30 12:09:38 +0100

    empty tiles added and styled

--- Frontend/tree-planters/src/components/ContentTiles.js
+++ Frontend/tree-planters/src/components/ContentTiles.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-export const ContentTiles = () => {
+const ContentTiles = () => {
     return (
-        <div>
+        <div className="ContentTiles">
+            <div className="Tile" style={{backgroundColor:"#6cbe30"}}>
+                
+                <img src="https://c.stocksy.com/a/u8V100/z9/358044.jpg"/>
+            </div>
+            <div className="Tile" style={{backgroundColor:"#dbb95f"}}>
+                
+                <h2></h2>
+                <p></p>
+
+            </div>
+            <div className="Tile" style={{backgroundColor:"#432e14"}}>
+                
+                <h2></h2>
+                <p></p>
+
+            </div>
+            <div className="Tile" style={{backgroundColor:"#878533"}}>
+                
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuw_2Erm4m7-2kSCCvmQg2amK7Yr4K50InfQ&usqp=CAU"/>
+            </div>
             
         </div>
     )
 }
+
+export default ContentTiles
