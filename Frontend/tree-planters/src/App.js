d97c1431faf305d7327a528c24b4f3a97d98c263
Samy <samyadams11@live.co.uk>
2021-05-03 12:03:39 +0100

    Added imports to LoginRegisterModal and state for login popup

--- Frontend/tree-planters/src/App.js
+++ Frontend/tree-planters/src/App.js
@@ -2,8 +2,20 @@ import React from 'react';
 import StickyAppBar from './components/StickyAppBar';
 import DefaultDashboard from './domain/DefaultDashboard/DefaultDashboard';
 import ContentTiles from './components/ContentTiles';
+import LoginRegisterModal from './domain/LoginRegisterModal/LoginRegisterModal'
 
 function App() {
+
+	const [loginPopupOpen, setLoginPopupOpen] = React.useState(false);
+
+	const openLoginPopup = () => {
+		setLoginPopupOpen(true);
+	};
+
+	const closeLoginPopup = () => {
+		setLoginPopupOpen(false);
+	};
+
 	return (
 		<div className='App'>
 			{/* Main app 
@@ -21,6 +33,8 @@ function App() {
 			*/}
 
 			<StickyAppBar />
+
+			<LoginRegisterModal />
 
 			<DefaultDashboard />
 
---
+++ Frontend/tree-planters/src/domain/LoginRegisterModal/LoginRegisterModal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from '.../components/Forms/Form' //? Check path is okay
+import FormInput from '.../components/Forms/FormSubmit'
+import Modal from '.../components/Modal'
+import Tabs from '.../components/Tabs'
+
+const LoginRegisterModal = ({open}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LoginRegisterModal
