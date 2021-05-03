569da93bce8f1ca2bb06d176dc75d9664a9d4f7e
Samy <samyadams11@live.co.uk>
2021-05-03 10:42:27 +0100

    Added Tabs component

---
+++ Frontend/tree-planters/src/components/Tabs.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import MUITabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import LoginForms from './LoginForms';
+import RegisterForms from './RegisterForms';
+
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role='tabpanel'
+			hidden={value !== index}
+			id={`full-width-tabpanel-${index}`}
+			aria-labelledby={`full-width-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box p={3}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+}
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.any.isRequired,
+	value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+	return {
+		id: `full-width-tab-${index}`,
+		'aria-controls': `full-width-tabpanel-${index}`,
+	};
+}
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		backgroundColor: theme.palette.background.paper,
+		width: 500,
+	},
+}));
+
+export default function Tabs({ arrayOfTabs, children }) {
+	const classes = useStyles();
+	const theme = useTheme();
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	const handleChangeIndex = (index) => {
+		setValue(index);
+	};
+
+	return (
+		<div className={classes.root}>
+			<AppBar position='static' color='default'>
+				<MUITabs
+					value={value}
+					onChange={handleChange}
+					indicatorColor='primary'
+					textColor='primary'
+					variant='fullWidth'
+					aria-label='full width tabs example'
+				>
+					{arrayOfTabs.map((str, index) => (
+						<Tab label={str} {...a11yProps(index)} />
+					))}
+				</MUITabs>
+			</AppBar>
+
+			<SwipeableViews
+				axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+				index={value}
+				onChangeIndex={handleChangeIndex}
+				style={{ height: 400 }}
+			>
+				{children.map((child, index) => (
+					<TabPanel value={value} index={index} dir={theme.direction}>
+						<child />
+					</TabPanel>
+				))}
+			</SwipeableViews>
+		</div>
+	);
+}
