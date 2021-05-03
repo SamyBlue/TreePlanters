784ed7f0c841797bbcc07e2bfcfa91612d320734
Samy <samyadams11@live.co.uk>
2021-05-03 13:12:17 +0100

    Added some template register fields

--- Frontend/tree-planters/src/domain/LoginRegisterModal/RegisterPage.js
+++ Frontend/tree-planters/src/domain/LoginRegisterModal/RegisterPage.js
@@ -3,17 +3,24 @@ import Form from '../../components/Forms/Form';
 import FormSubmit from '../../components/Forms/FormSubmit';
 
 const RegisterPage = () => {
-    const [formFields, setFormFields] = React.useState({
+	const [formFields, setFormFields] = React.useState({
 		Username: '',
 		'Password*': '',
+		'Confirm Password*': '',
 		'Postal Code': '',
-    });
-    
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	});
 
-export default RegisterPage
+	return (
+		<div>
+			<h2>Welcome</h2>
+			<Form formFields={formFields} setFormFields={setFormFields} />
+			<FormSubmit
+				label='Register'
+				attemptSubmitMsg='Registering...'
+				failedSubmitMsg="Some of your information isn't correct. Please try again."
+			/>
+		</div>
+	);
+};
+
+export default RegisterPage;
