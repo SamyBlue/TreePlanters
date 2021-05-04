670466a160a4b4d68c8cf3e1d4bf579c31680789
Samy <samyadams11@live.co.uk>
2021-05-04 16:01:16 +0100

    Made cards circular

--- Frontend/tree-planters/src/domain/DefaultDashboard/DefaultDashboard.js
+++ Frontend/tree-planters/src/domain/DefaultDashboard/DefaultDashboard.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-import LooksOne from '@material-ui/icons/LooksOne';
-import LooksTwo from '@material-ui/icons/LooksTwo';
-import Looks3 from '@material-ui/icons/Looks3';
 import './DefaultDashboard.css';
 
 const TREES_PLANTED = 1000; //! TEMP VALUE
@@ -47,38 +44,23 @@ const DefaultDashboard = ({ openDonateModal }) => {
 				<li>
 					{/* <img src='growingtree.gif' alt='growing tree' /> */}
 					<div className='DashboardCard'>
-						<LooksOne />
-						<p>Trees Planted:</p>
+						<p>{`Trees
+						Planted`}</p>
 						<p>{TREES_PLANTED}</p>
-						<p>
-							200,000 acres of rainforest are burned every day. This is why
-							planting trees now is so important. Plant for a better tomorrow!
-						</p>
 					</div>
 				</li>
 				<li>
 					{/* <img src='foot.png' alt='carbon footprint' /> */}
 					<div className='DashboardCard'>
-						<LooksTwo />
-						<p>Carbon Saved:</p>
+						<p>Carbon Saved</p>
 						<p>{CARBON_SAVED} kg</p>
-						<p>
-							A mature tree absorbs carbon dioxide at a rate of 21.8 kg per
-							year. In one year, an acre of forest can absorb twice the CO2
-							produced by the average car's annual mileage.
-						</p>
 					</div>
 				</li>
 				<li>
 					{/* <img src='pig.png' alt='piggy bank' /> */}
 					<div className='DashboardCard'>
-						<Looks3 />
-						<p>Money Raised:</p>
+						<p>Money Raised</p>
 						<p>£{MONEY_RAISED}</p>
-						<p>
-							Every pound matters! 100% of our profits go towards planting more
-							trees and making the world a more leafy place.
-						</p>
 					</div>
 				</li>
 			</ul>
